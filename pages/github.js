d9a75cc3d02c70d54b0faf6bced501b2de6376bf
Yasir <89215938+yasirkaramandev@users.noreply.github.com>
2024-05-16 23:46:22 +0300

    Create github.js

---
+++ pages/github.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Github = () => {
+  return (
+    <div>
+      <h1>GitHub&apos;s Page</h1>
+      <p>This is an example page for GitHub&apos;s link.</p>
+    </div>
+  );
+};
+
+export default Github;
