d9b1027f1917ae3fefbb6ee2a5fb1794b8fcdde1
Yasir <89215938+yasirkaramandev@users.noreply.github.com>
2024-05-21 22:41:30 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,15 @@ const HomePage = () => {
     <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
       <h1>Yapım Aşamasında</h1>
     </div>
+    <div>
+  <a href="https://www.linkedin.com/in/sizin-linkedin-hesabınız" target="_blank" rel="noopener noreferrer">
+    <i className="fab fa-linkedin"></i>
+  </a>
+  <a href="https://github.com/sizin-github-hesabınız" target="_blank" rel="noopener noreferrer">
+    <i className="fab fa-github"></i>
+  </a>
+</div>
+
   );
 };
 
