7cbcd1e4c26a7f723bfe1e18d2661ab92c65709d
Yasir <89215938+yasirkaramandev@users.noreply.github.com>
2024-05-21 22:48:47 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,11 +5,11 @@ const HomePage = () => {
     <div className="container mx-auto flex flex-col items-center justify-center h-screen">
       <h1 className="text-3xl mb-6">Yapım Aşamasında</h1>
       <div className="flex space-x-4">
-        <a href="https://www.linkedin.com/in/sizin-linkedin-hesabınız" target="_blank" rel="noopener noreferrer" className="text-gray-600 hover:text-gray-900">
-          <i className="fab fa-linkedin fa-2x"></i>
+        <a href="https://www.linkedin.com/in/yasirkaraman/" target="_blank" rel="noopener noreferrer" className="text-blue-600 hover:underline hover:text-blue-800">
+          LinkedIn
         </a>
-        <a href="https://github.com/sizin-github-hesabınız" target="_blank" rel="noopener noreferrer" className="text-gray-600 hover:text-gray-900">
-          <i className="fab fa-github fa-2x"></i>
+        <a href="https://github.com/yasirkaramandev" target="_blank" rel="noopener noreferrer" className="text-blue-600 hover:underline hover:text-blue-800">
+          GitHub
         </a>
       </div>
     </div>
