d233e19b2d2255f730dfc44b57e3dd929f491439
Yasir <89215938+yasirkaramandev@users.noreply.github.com>
2024-05-22 21:46:19 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
-import styles from './styles/Home.module.css';
+import styles from '../styles/Home.module.css';
 
 const HomePage = () => {
   return (
