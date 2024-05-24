3772e4d543982aeaa96540c750624d47f47ec7ab
Yasir <89215938+yasirkaramandev@users.noreply.github.com>
2024-05-24 18:28:46 +0300

    Add files via upload

---
+++ nextjstry/pages/linkedln.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Linkedln = () => {
+  return (
+    <div>
+      <h1>LinkedIn&apos;s Page</h1>
+      <p>This is an example page for LinkedIn&apos;s link.</p>
+    </div>
+  );
+};
+
+export default Linkedln;
