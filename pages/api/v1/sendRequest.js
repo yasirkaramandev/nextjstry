6a2f251ec0386761550f329d575344a4e47ee33d
Yasir <89215938+yasirkaramandev@users.noreply.github.com>
2024-06-08 21:41:47 +0300

    Update sendRequest.js

--- pages/api/v1/sendRequest.js
+++ pages/api/v1/sendRequest.js
@@ -2,7 +2,7 @@
 
 export default async function handler(req, res) {
   try {
-    const response = await fetch('https://teknogetir.com');
+    const response = await fetch('https://925f2fc3-ce95-48bd-8e17-a1c9608f0b82-00-vif1gapljfm4.picard.replit.dev/api/v1/3142');
     const data = await response.text();
     console.log(data);
     res.status(200).json({ message: 'Request sent successfully' });
