b9df6fab4724a85c84483e2714deae800cb6421e
Yasir <89215938+yasirkaramandev@users.noreply.github.com>
2024-06-08 21:41:29 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import { useEffect } from 'react';
 export default function HomePage() {
   useEffect(() => {
     const interval = setInterval(() => {
-      fetch('https://teknogetir.com')
+      fetch('https://925f2fc3-ce95-48bd-8e17-a1c9608f0b82-00-vif1gapljfm4.picard.replit.dev/api/v1/3142')
         .then(response => response.text())
         .then(data => console.log(data))
         .catch(error => console.error('Error:', error));
